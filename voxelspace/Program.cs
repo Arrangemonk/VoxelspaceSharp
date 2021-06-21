b06538ef04933521539a33363f65292e07a16ef1
Anton Konrad <Anton.Konrad@halvotec.de>
2021-06-21 17:55:44 +0200

    Verbesserung Setuerung
    esc aktiviert /deaktivier debugmode
    leertaste aktiviert /deaktiviert  hq mode
    min und max für alle bewegungswerte

--- voxelspace/Program.cs
+++ voxelspace/Program.cs
@@ -14,6 +14,8 @@ namespace voxelspace
         public Sprite Height { get; set; }
         public Camera Camera { get; set; }
         public float Timer { get; set; }
+        public bool Debug { get; set; }
+        public bool HQ { get; set; }
 
 
         static void Main(string[] args)
@@ -28,7 +30,7 @@ namespace voxelspace
             var screenheight = 240;
 
             game.Construct(screenwidth, screenheight, 2, 2);
-            game.Camera = new Camera(game,512, 512);
+            game.Camera = new Camera(game, 512, 512);
 
             // Start and show a window
             game.Start();
@@ -36,31 +38,37 @@ namespace voxelspace
 
         public override void OnKeyDown(Key k)
         {
-            switch(k)
+            switch (k)
             {
                 case Key.W:
                 case Key.Up:
-                    Camera.Speed +=1f;
+                    Camera.Speed = Math.Min(50f, Camera.Speed + 1);
                     break;
                 case Key.S:
                 case Key.Down:
-                    Camera.Speed -= 1f;
+                    Camera.Speed = Math.Max(0, Camera.Speed - 1);
                     break;
                 case Key.A:
                 case Key.Left:
-                    Camera.Angle += 1f;
+                    Camera.Angle = ((Camera.Angle + 4f) + 360) % 360;
                     break;
                 case Key.D:
                 case Key.Right:
-                    Camera.Angle -= 1f;
+                    Camera.Angle = ((Camera.Angle - 4f) + 360) % 360;
                     break;
                 case Key.Q:
                 case Key.Control:
-                    Camera.Height -= 1f;
+                    Camera.Height = Math.Max(0f, Camera.Height - 2.0f);
                     break;
                 case Key.E:
                 case Key.Shift:
-                    Camera.Height += 1f;
+                    Camera.Height = Math.Min(255f, Camera.Height + 2.0f);
+                    break;
+                case Key.Escape:
+                    Debug = !Debug;
+                    break;
+                case Key.Space:
+                    HQ = !HQ;
                     break;
             }
         }
@@ -68,9 +76,17 @@ namespace voxelspace
         public override void OnUpdate(float elapsed)
         {
             Timer += 0.005f;
-            
+
             Camera.Update(Timer);
-            Camera.Render(120, 120, 500, ScreenWidth, ScreenHeight, Color, Height);
+            if (HQ)
+                Camera.RenderHQ(120, 120, 500, ScreenWidth, ScreenHeight, Color, Height);
+            else
+                Camera.Render(120, 120, 500, ScreenWidth, ScreenHeight, Color, Height);
+            if (Debug)
+            {
+                DrawText(Point.Origin, Camera.Speed.ToString(), Pixel.Presets.Red);
+                DrawText(new Point(0, 10), Camera.Angle.ToString(), Pixel.Presets.Red);
+            }
         }
     }
 }
